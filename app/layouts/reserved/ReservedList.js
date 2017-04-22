f1de8562645e6c522a4690ebfa7763c5a747692e
Tri Truong <tri.itvn@gmail.com>
2017-04-23 06:22:40 +0700

    Bugfix: reverved list

--- app/layouts/reserved/ReservedList.js
+++ app/layouts/reserved/ReservedList.js
@@ -11,6 +11,7 @@ import Styles from './styles'
 import Images from '../../config/Images'
 // import {connect} from 'react-redux';
 // import {actionCreators} from "../../reducer/reducer";
+import BookingSuccess from './../bookingSuccess/bookingSuccess';
 
 const list = [{name: 'Parking Heaven', address: '208 Điện Biên Phủ', in_out: '10h - 17h', image: Images.CoverImage, date: '23/4/2017'},
   {name: 'Hell Parking', address: '11 Lê Duẩn', in_out: '8h - 20h', image: Images.reservedIcon, date: '23/4/2017'},
@@ -56,18 +57,18 @@ export default class ReservedList extends Component {
           <Body>
           <Title>Danh sách đặt chỗ</Title>
           </Body>
-          <Right>
-            <Button transparent>
-              <Icon name='share' />
-            </Button>
-          </Right>
         </Header>
         <Content>
           <List>
-            {list.map(function (obj) {
+            {list.map(function (obj, index) {
               return (
-                <Card>
-                  <CardItem>
+                <Card key={index}>
+                  <CardItem button onPress={() => {
+                    navigator.push({
+                      title: "Booking success",
+                      component: BookingSuccess
+                    })
+                  }}>
                     <Thumbnail square size={80} source={obj.image} />
                     <View style={Styles.itemBody}>
                       <Text style={Styles.title}>{obj.name}</Text>
