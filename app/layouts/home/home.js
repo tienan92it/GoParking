f1092061343a59db9ae81112c993cec7c0bca167
Tri Truong <tri.itvn@gmail.com>
2017-04-22 12:59:59 +0700

    Inject into view

--- app/layouts/home/home.js
+++ app/layouts/home/home.js
@@ -2,12 +2,27 @@
  * Created by AnTran on 4/22/17.
  */
 import React, { Component } from 'react';
+import MapView from 'react-native-maps';
 import {
   View,
   TextView,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
-import Styles from './styles'
+import Styles from './styles';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 // import {connect} from 'react-redux';
 // import {actionCreators} from "../../reducer/reducer";
@@ -20,7 +35,18 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Text>lorem ipsum</Text>
+      <View style ={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+      </View>
     )
   }
 }
