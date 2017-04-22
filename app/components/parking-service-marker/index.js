a03bc547c8eef14afcb2c14ab46facb3d37b28d2
Tri Truong <tri.itvn@gmail.com>
2017-04-22 15:09:44 +0700

    Add my location icon

--- app/components/parking-service-marker/index.js
+++ app/components/parking-service-marker/index.js
@@ -4,6 +4,7 @@ import {
   Text, Icon
 } from 'native-base';
 import Styles from './styles';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default class ParkingServiceMarker extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class ParkingServiceMarker extends Component {
       <View style={Styles.parkingServiceMarker}>
         <Icon name="ios-pin" />
         <Text style={Styles.pricing}>20$</Text>
+        <MaterialIcons name="my-location" />
       </View>
     );
   }
